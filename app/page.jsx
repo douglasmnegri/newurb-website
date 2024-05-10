12df0a280ccd654a576f332ec2864da1080b259a
Douglas Melo <douglasmnegri@gmail.com>
2024-05-09 21:28:09 -0300

    add background and text of hero section

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Land from "@/components/Land";
+import Release from "@/components/Release";
+
 export default function Home() {
   return ( 
   <>
-  
+  <Hero />
+  <Features />
+  <Land />
+  <Release />
   </>
   );
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative">
+      <div className="hidden lg:block relative">
+        <div className="bg-cover bg-no-repeat h-dvh bg-fixed bg-center relative">
+          <Image
+            src="/family.jpg"
+            alt="family"
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-start items-center text-white">
+          <h1 className="font-bold text-3xl lg:text-5xl text-center">
+            Urbanism as never seen before.
+          </h1>
+          <p className="text-base lg:text-lg mt-6 text-gray-700 xl:max-w-[520px] text-center">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime
+            illum consectetur fugiat repellendus, explicabo ipsa quas facere
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
